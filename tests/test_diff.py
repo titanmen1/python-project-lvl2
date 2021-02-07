4eb74c5758a8b0cdfaac728b86ea854622991a27
Artem Arkhipov <arkhipov@algomost.ru>
2021-02-07 21:29:13 +0300

    add test

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -7,3 +7,17 @@ def test_gendiff_json():
     file2 = './tests/fixtures/file2.json'
     output = './tests/fixtures/output.json'
     assert generate_diff(file1, file2, 'json') == open(output, 'r').read()
+
+
+def test_gendiff_input_yaml():
+    file1 = './tests/fixtures/file1.yml'
+    file2 = './tests/fixtures/file2.yml'
+    output = './tests/fixtures/output.yml'
+    assert generate_diff(file1, file2, 'json') == open(output, 'r').read()
+
+
+def test_gendiff_output_format_plain():
+    file1 = './tests/fixtures/file1.yml'
+    file2 = './tests/fixtures/file2.yml'
+    output = './tests/fixtures/test_plain'
+    assert generate_diff(file1, file2, 'plain') == open(output, 'r').read()
